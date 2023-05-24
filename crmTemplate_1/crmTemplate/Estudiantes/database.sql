d4189f1be36817d58f7b85bed4d405175116e3fd
LuisFelipeRuedaGarcia <lfxrg0724@gmail.com>
2023-05-24 08:41:36 -0500

    Revert "x"
    This reverts commit 2f80cc4d009c6b737bfcbda300d543e1bd8efcb0.

--- crmTemplate_1/crmTemplate/Estudiantes/database.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE campus;
-
-CREATE TABLE campers(
-    id INT primary key AUTO_INCREMENT,
-    NOMBRES VARCHAR (50) NOT NULL,
-    direccion VARCHAR (50),
-    logros VARCHAR (60)
-);
-
